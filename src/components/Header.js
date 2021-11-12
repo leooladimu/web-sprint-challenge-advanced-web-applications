e06ff6abf8687000cc0812d571fa0b1a48650121
Leo Oladimu <leooladimu9@gmail.com>
2021-11-12 17:54:16 -0500

    3

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,7 +5,7 @@ import styled from 'styled-components';
 const Header = () => {
     return(
         <HeaderStyle>
-            <p>Blogger Pro</p>
+            <p>Blogger Pro  〄</p>
             <MenuStyle>
                 <li><Link to="/">Login</Link></li>
                 <li><Link to="view">View</Link></li>
--- src/utils/axiosWithAuth.js
+++ src/utils/axiosWithAuth.js
@@ -1,7 +1,13 @@
 import axios from 'axios';
 
 const axiosWithAuth = ()=> {
-    
+    const token = localStorage.getItem('token');
+    return axios.create({
+        baseURL: 'http://localhost:3000',
+        headers: {
+            Authorization: token
+        }
+    })
 }
 
 export default axiosWithAuth;
